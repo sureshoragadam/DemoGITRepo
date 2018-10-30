d6b1384c5e31ae6e1481a08b1c53958e37af58b0
sureshoragadam <44584042+sureshoragadam@users.noreply.github.com>
2018-10-30 12:04:03 +0530

    added search page class and modified login page class

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,7 +2,16 @@ package com.qa.pages;
 
 public class LoginPage {
 	public void loginPage() {
-		System.out.println("inside login page");
+		System.out.println("inside login page");		
+		
+	}
+	
+	public void setusername() {
+		System.out.println("set username");
+	}
+	
+	public void setpassword() {
+		System.out.println("set password");
 	}
 	
 }
