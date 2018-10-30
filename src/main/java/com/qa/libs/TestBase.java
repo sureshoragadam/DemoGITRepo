82ae3cbe3482e187d7061280c230f3a63874b582
sureshoragadam <44584042+sureshoragadam@users.noreply.github.com>
2018-10-30 11:28:29 +0530

    first repository commit of my maven project

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.libs;
+
+public class TestBase {
+	public void testbase() {
+		System.out.println("inside the test base class");
+	}
+	
+}
---
+++ src/main/java/com/qa/utilities/TestUtil.java
@@ -0,0 +1,8 @@
+package com.qa.utilities;
+
+public class TestUtil {
+	public void testUtil() {
+		System.out.println("inside testUtil");
+	}
+
+}
