59df1dbcabf9f5f9e73810bca4229d7eb27a3df1
sureshoragadam <44584042+sureshoragadam@users.noreply.github.com>
2018-10-30 13:12:00 +0530

    Update SearchPage.java

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -3,6 +3,8 @@ package com.qa.pages;
 public class SearchPage {
 	public void searchpage() {
 		System.out.println("i am in search page");
+		System.out.println("i am in search page and updated file");
+		
 	}
 	
 
